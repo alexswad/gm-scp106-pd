056fe5996a10add6b4d6e76197d3a2a3fcb29a54
ShakesW <gameboycrafter@gmail.com>
2025-02-21 11:19:05 -0500

    Update ent_106pd_puddle.lua

--- lua/entities/ent_106pd_puddle.lua
+++ lua/entities/ent_106pd_puddle.lua
@@ -26,7 +26,11 @@ end
 
 function ENT:StartTouch(ply)
 	if self.Closing or not ply:IsPlayer() and not ply:IsNPC() then return end
-	if ply:IsNPC() and not pd106.class_106[ply:GetClass()] then pd106.PutNPCInPD(ply, self) return end
+	if ply:IsNPC() then
+		if pd106.class_106[ply:GetClass()] then return end
+		pd106.PutNPCInPD(ply, self)
+		return
+	end
 	pd106.PutInPD(ply, self)
 end
 
